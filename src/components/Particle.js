f457c7fe086cdfaf496aa16dbaabcac3ad073554
Sivadharshan V <sivadharshan980@gmail.com>
2023-06-17 22:32:22 +0530

    Stars Updted

--- src/components/Particle.js
+++ src/components/Particle.js
@@ -1,10 +1,26 @@
 import React from "react";
+import { useCallback } from "react";
 import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 
-function Particle() {
-  return (
+function Particle()
+{
+
+  const particlesInit = useCallback(async engine => {
+    console.log(engine);
+    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+}, []);
+
+const particlesLoaded = useCallback(async container => {
+    await console.log(container);
+}, []);
+
+  return(
     <Particles
-      id="tsparticles"
+    id="tsparticles"
       params={{
         particles: {
           number: {
@@ -49,7 +65,7 @@ function Particle() {
         retina_detect: true,
       }}
     />
-  );
+  )
 }
 
-export default Particle;
+export default Particle
