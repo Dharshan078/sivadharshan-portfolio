c8f44d9b88dbec83038e6c879d6d81910c4b38bb
Sivadharshan V <sivadharshan980@gmail.com>
2023-06-17 22:37:53 +0530

    Star Particles updated

--- src/components/Particle.js
+++ src/components/Particle.js
@@ -1,22 +1,9 @@
 import React from "react";
-import { useCallback } from "react";
 import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
+
 
 function Particle()
 {
-
-  const particlesInit = useCallback(async engine => {
-    console.log(engine);
-    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull(engine);
-}, []);
-
-const particlesLoaded = useCallback(async container => {
-    await console.log(container);
-}, []);
 
   return(
     <Particles
