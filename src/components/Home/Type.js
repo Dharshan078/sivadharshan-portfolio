8705846cb6abce745424ed587b4e4ebb165fde82
Sivadharshan V <sivadharshan980@gmail.com>
2023-06-19 22:21:23 +0530

    Improved typewritter effect

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -10,7 +10,7 @@ function Type() {
           "Electrical & Electronics Engineer",
           "UX-UI Designer",
           "Open Source Contributor",
-          "Embedded Developer",
+          "Embedded Designer",
         ],
         autoStart: true,
         loop: true,
